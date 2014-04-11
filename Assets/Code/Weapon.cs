b17a06ec92e58fedb10be3d14a8ec3ce60f23bc3
Anders Olsen <TheFracker@gmail.com>
2014-04-11 20:14:19 +0200

    Created Weapon and Wrahh class

---
+++ Assets/Code/Weapon.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	string name;
+	int ammo;
+	float reloadTime;
+	bool reloading;
+	int durability;
+	float accidentalFire;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void hit()
+	{
+		Debug.Log ("Hitting");
+	}
+
+	public void shoot()
+	{
+		Debug.Log ("Shooting");
+	}
+
+	public void reload()
+	{
+		Debug.Log ("Reloading");
+	}
+
+	public bool isReloading()
+	{
+		if (reloading)
+			return true;
+		return false;
+	}
+}

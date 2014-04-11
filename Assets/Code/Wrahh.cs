5aaeed715df88f76e55b33e1f74b444f82f20949
Anders Olsen <TheFracker@gmail.com>
2014-04-11 21:54:29 +0200

    Wrahh is now able to move and throw grenades

--- Assets/Code/Wrahh.cs
+++ Assets/Code/Wrahh.cs
@@ -8,17 +8,34 @@ public class Wrahh : MonoBehaviour {
 	Weapon[] weapons;
 	Weapon currentWeapon;
 	int grenades;
+	float moveSpeed = 5.0f;
+	float MAX_MOVE_SPEED = 1.0f;
 
 	// Use this for initialization
 	void Start () {
-		health = 3; // Three hearts
+		health = 3;  // Three hearts
 		armor = 0; // No armor to start with
-		grenades = 0; // Haven't found any grenades yet
+		grenades = 0; // Nothing to throw yet
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// movement in here
+		// Throw grenade
+		if (Input.GetKeyUp(KeyCode.G))
+			throwGrenade ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis ("Horizontal");
+
+		// Moving right
+		if (h * rigidbody2D.velocity.x < MAX_MOVE_SPEED)
+			rigidbody2D.AddForce (Vector2.right * h * moveSpeed);
+
+		// Moving left
+		if (Mathf.Abs(h * rigidbody2D.velocity.x) > MAX_MOVE_SPEED)
+			rigidbody2D.AddForce (Vector2.right * h * moveSpeed);
 	}
 
 	public void useWeapon(Weapon currentWeapon)
@@ -29,9 +46,11 @@ public class Wrahh : MonoBehaviour {
 	public void throwGrenade()
 	{
 		if (grenades > 0) {
-			Debug.Log("Throwing grenade");
+			Debug.Log("Throwing grenade " + grenades);
 			grenades--;
+			return;
 		}
+		Debug.Log ("Don't have anything to throw");
 	}
 
 	public void die()
@@ -39,14 +58,14 @@ public class Wrahh : MonoBehaviour {
 		Debug.Log ("Dying");
 	}
 
-	public void OnCollisionEnter(Collider2D c)
+	void OnCollisionEnter2D(Collision2D c)
 	{
 		// Pick up weapon
-		if (c.tag == "Weapon")
+		if (c.collider.tag == "Weapon")
 			// picks up weapon
 			Debug.Log ("Picking up this thing");
 		// Pick up armor
-		if (c.tag == "Armor")
+		if (c.collider.tag == "Armor")
 			// picks up armor
 			Debug.Log ("This can protect me");
 	}

51ca6a3ad334d6cb1f29a4be27a41a1322a8246c
Nadia Axel <nrasmu12@student.aau.dk>
2014-04-12 15:15:03 +0200

    Working on Lobster's ability to chase the player!

--- Assets/Code/Lobster.cs
+++ Assets/Code/Lobster.cs
@@ -3,16 +3,21 @@ using System.Collections;
 
 public class Lobster : MonoBehaviour
 {
-	public int speed = 4;
-	
-	void Start ()
+	public float speed = 4.0f;
+	public float maxDistance = 15.0f;
+
+	Transform target;
+	Transform enemyTransform;
+
+	void Start()
 	{
-	
+		enemyTransform = this.GetComponent<Transform>();
 	}
 
 	void Update ()
 	{
-
+		target = GameObject.FindWithTag("Player").transform;			//Look for target
+		enemyTransform.Translate(speed*Time.deltaTime, 0, 0);
 	}
 
 	void OnTriggerEnter2D(Collider2D c)

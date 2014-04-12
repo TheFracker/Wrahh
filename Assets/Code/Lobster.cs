bab73683795237d12f584aec6d9eae9b4d684456
Nadia Axel <nrasmu12@student.aau.dk>
2014-04-12 13:50:27 +0200

    Made class for Lobster

---
+++ Assets/Code/Lobster.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lobster : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.LookAt (Wrahh);	//Referring to the Player.
+	}
+}

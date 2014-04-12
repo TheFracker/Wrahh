a289e4b6d3ebfb4befa80c809e552959a81d7d64
Nadia Axel <nrasmu12@student.aau.dk>
2014-04-12 16:01:25 +0200

    The lobster can chase the Player!

--- Assets/Code/Lobster.cs
+++ Assets/Code/Lobster.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class Lobster : MonoBehaviour
 {
 	public float speed = 4.0f;
-	public float maxDistance = 15.0f;
+	public float maxDist = 15.0f;
 
 	Transform target;
 	Transform enemyTransform;
-
+	
 	void Start()
 	{
 		enemyTransform = this.GetComponent<Transform>();
@@ -17,14 +17,30 @@ public class Lobster : MonoBehaviour
 	void Update ()
 	{
 		target = GameObject.FindWithTag("Player").transform;			//Look for target
-		enemyTransform.Translate(speed*Time.deltaTime, 0, 0);
+
+		if (Vector3.Distance(target.position, enemyTransform.position) < maxDist)
+		{
+			Vector3 dir = target.position - enemyTransform.position;
+			dir.Normalize();
+			enemyTransform.position += dir * speed * Time.deltaTime;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D c)
 	{
 		if (c.tag == "Player")
 		{
-			Debug.Log("Lobster sight triggered!");
+			Debug.Log("Lobster touched Wrahh!");
+
+			enemyTransform.position += 0 * speed * Time.deltaTime
+
+			//---
+			//	Wrahh stops moving
+			//	Lobster stops moving
+			//	Wrahh should die
+			//	Wrahh should respwan
+			//---
+			Debug.Log("Game Over!");
 		}
 	}
 }

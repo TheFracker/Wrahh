3bbff2fb4884a30829b4fb94d92ce111be421b95
skarregaard89 <skarregaard89@gmail.com>
2014-04-13 17:01:46 +0200

    You can now use arrow keys to use monkey bars

--- Assets/Code/MonkeyBars.cs
+++ Assets/Code/MonkeyBars.cs
@@ -2,25 +2,20 @@
 using System.Collections;
 
 public class MonkeyBars : MonoBehaviour {
-
-
-	private bool triggerActive = false;
-	public static bool onMonkeyBar;
-	private Animator anim;
-
-	// Use this for initialization
-	void Start () {
 	
-	}
+	private bool triggerActive = false; //bool to check wether the trigger area have been entered 
+	public static bool onMonkeyBar; //bool used to check if the button is active and to send on to "Wrahh.cs"
 	
 	// Update is called once per frame
 	void Update () {
 	
-		if(Input.GetKeyDown(KeyCode.W) && triggerActive == true){
-			onMonkeyBar = true;
+		// If "W" is pressed and the trigger is active/the player is in the trigger zone
+		if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) && triggerActive == true){
+			onMonkeyBar = true; //bolean set to true and used in (Wrahh.cs)
 		}
 
-		if(Input.GetKeyDown(KeyCode.S)){
+
+		if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
 			onMonkeyBar = false;
 		}
 

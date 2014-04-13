f061b04eb4b3bd2908231c7afbdd2daedad62c42
Nadia Axel <nrasmu12@student.aau.dk>
2014-04-13 20:37:29 +0200

    Main menu scene is done and other kewl stuff

---
+++ Assets/Code/playButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playButton : MonoBehaviour
+{
+	public Texture2D playNormal;
+	public Texture2D playHover;
+
+	void OnMouseEnter()
+	{
+		guiTexture.texture = playHover;
+	}
+
+	void OnMouseExit()
+	{
+		guiTexture.texture = playNormal;
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log("Load first level...");
+		//Application.LoadLevel("level-1");	//This line is ready for use!
+	}
+}

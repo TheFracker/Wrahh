115bb2545d0058ae877bd581efc540fdc609f74f
skarregaard89 <skarregaard89@gmail.com>
2014-04-13 13:09:25 +0200

    Monkeybars sprite and the start of the first code for it

---
+++ Assets/Code/MonkeyBars.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonkeyBars : MonoBehaviour {
+
+
+	private bool triggerActive = false;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if(other.tag == "Player" && triggerActive == false){
+			triggerActive = true;
+			Debug.Log("Trigger is active");
+			//set this in wrahh:::: anim.SetBool("crawling", true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		
+		if(other.tag == "Player" && triggerActive == true){
+			triggerActive = false;
+			Debug.Log("Trigger is not active");
+		}
+	}
+
+
+}

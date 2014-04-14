e82f0687415b71592dd29a43e3365b1fcf450ba7
Anders Olsen <TheFracker@gmail.com>
2014-04-14 15:33:24 +0200

    Small changes to Projectile

--- Assets/Code/HitProjectile.cs
+++ Assets/Code/HitProjectile.cs
@@ -5,25 +5,17 @@ public class HitProjectile : Projectile {
 
 	float liveTime;
 	float timeToDie;
-	bool start;
 
 	// Use this for initialization
 	void Start () {
 		speed = 0;
 		damage = 1;
 		liveTime = 1.0f;
-		start = false;
+		timeToDie = Time.time + liveTime;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (!start)
-		{
-			timeToDie = Time.time + liveTime;
-			start = true;
-		}
-
-		Debug.Log (Time.time - timeToDie);
 
 		if(timeToDie < Time.time)
 		{

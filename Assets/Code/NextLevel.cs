5a2f19808600e05365bef218501f9420e0804a9d
skarregaard89 <skarregaard89@gmail.com>
2014-04-14 14:36:38 +0200

    jail animation

---
+++ Assets/Code/NextLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Player")
+			Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}

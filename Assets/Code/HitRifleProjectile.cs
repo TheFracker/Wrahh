005bdf51a4fa975e15525bc415be6395ff100cf6
Anders Olsen <TheFracker@gmail.com>
2014-04-25 18:22:43 +0200

    There are now projectiles for guns and rifles when Wrahh uses these weapons to hit with

---
+++ Assets/Code/HitRifleProjectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitRifleProjectile : HitProjectile {
+
+	// Use this for initialization
+	void Start () {
+		speed = 0;
+		damage = 3;
+		liveTime = 1.0f;
+		timeToDie = Time.time + liveTime;
+		criticalChange = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timeToDie < Time.time)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}

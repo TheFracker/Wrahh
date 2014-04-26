584a6fbe3a1e7ccefd8381e744577037dd63ada3
Nadia Axel <nrasmu12@student.aau.dk>
2014-04-26 19:11:51 +0200

    Making gameoverSCcreen

--- Assets/Code/Lobster.cs
+++ Assets/Code/Lobster.cs
@@ -67,7 +67,7 @@ public class Lobster : GameCharacters
 		{
 			GameObject itemDrop = (GameObject)GameObject.Instantiate(lobsterPart, Vector2.zero, Quaternion.identity); // Instanciate a new gameobject based on the prefab of the lobster parts
 			itemDrop.transform.Translate(enemyTransform.position.x + 1, enemyTransform.position.y + 1.5f, enemyTransform.position.z); // Place the dropped item at the lobster's last known position with a little offset
-			die(this.gameObject);																			// Remove the dead lobster from the game
+			die(this.gameObject);															// Remove the dead lobster from the game
 		}
 	}
 

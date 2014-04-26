c559a0a3df048ca2397808648e2a6f18ba123453
Nadia Axel <nrasmu12@student.aau.dk>
2014-04-26 20:51:11 +0200

    gameoverScreen DONE!

--- Assets/Code/GUI_controller.cs
+++ Assets/Code/GUI_controller.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class GUI_controller : MonoBehaviour
 {
-	void FixedUpdate()
-	{
-		if (GameCharacters.playerDead)
-			Debug.Log("HE IS DEEEAAADDDDD!!!");
+	public GameObject gameoverScreen;
+	Quaternion GUIrot = Quaternion.identity;
+	Vector3 GUIpos = new Vector3(7.5f, 4.5f, -1.3f);
 
+	void Update()
+	{
+		if (Lobster.playerDead)
+		{
+			Instantiate(gameoverScreen, GUIpos, GUIrot);
+			Lobster.playerDead = false;
+		}
+			
 	}
 }

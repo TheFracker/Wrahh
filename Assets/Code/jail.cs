2fb421a99f157793b4d4989c2b18f430d8ea6c86
skarregaard89 <skarregaard89@gmail.com>
2014-04-26 13:21:07 +0200

    Jail sound

--- Assets/Code/jail.cs
+++ Assets/Code/jail.cs
@@ -3,27 +3,25 @@ using System.Collections;
 
 public class jail : MonoBehaviour {
 
-	Animator anim;
+
+	Animator anim;							// creates an variable "anim" of type "Animator"
+	AudioSource[] sounds; 					// creates an array "sounds" of type "AudioSource"
+	AudioSource jailDoor;					// creates an variable "jailDoor" of type "AudioSource"
 
 	// Use this for initialization
 	void Start () {
-	
+		sounds = GetComponents<AudioSource>();		//all audio source components on the object it put in the "sounds" array in the order they are listed on the object
+		jailDoor = sounds[0];						//the different audio sources in the "sounds" array is initialized in a variable to create a better overwiev
+		anim = GetComponent<Animator>();			//The animator component on the object is accessed through anim
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-
-
-	void OnCollisionEnter2D(Collision2D other)
+	void OnCollisionEnter2D(Collision2D other)		//checks for collision
 	{
-		if(other.collider.tag == "Player")
+		if(other.collider.tag == "Player" && anim.GetBool("DoorHit") == false)			//cheks if an object with "player" tag collides with the object
 		{
-
-			anim = GetComponent<Animator>();
-			anim.SetBool("DoorHit", true);
+			anim.SetBool("DoorHit", true);			//sets the boolean parameter "DoorHit" in the animator to true
+			jailDoor.Play();					//plays the audio in the varible "jailDoor". Could also be played by sounds[0].PlayOneShot
 		}
 			
 	}

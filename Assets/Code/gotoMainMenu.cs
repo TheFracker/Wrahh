2a390dca449e5987fda0b0962fa6b05a0858df4f
Nadia Axel <nrasmu12@student.aau.dk>
2014-04-26 19:53:10 +0200

    Made code for gameover screen buttons

---
+++ Assets/Code/gotoMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gotoMainMenu : MonoBehaviour
+{
+	public Texture2D mainMenu_btn, mainMenuHover_btn;
+	
+	void OnMouseEnter()
+	{
+		guiTexture.texture = mainMenuHover_btn;
+	}
+	
+	void OnMouseExit()
+	{
+		guiTexture.texture = mainMenu_btn;
+	}
+	
+	void OnMouseDown()
+	{
+		Application.LoadLevel(0);
+	}
+}

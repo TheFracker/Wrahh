ae1502c385e197a0f20eddbec43becc13a1b9503
skarregaard89 <skarregaard89@gmail.com>
2014-04-27 20:59:22 +0200

    WorldSound and NextLevel comment

--- Assets/Code/NextLevel.cs
+++ Assets/Code/NextLevel.cs
@@ -1,16 +1,18 @@
-﻿using UnityEngine;
+﻿/// <summary>
+/// Is added to an empty gameobject with an trigger whereever it is wanted that the player should proceed to the next level
+/// </summary>
+
+using UnityEngine;
 using System.Collections;
 
-public class NextLevel : MonoBehaviour {
-
-	// Changes to the next level
-	void OnTriggerEnter2D (Collider2D other)
+public class NextLevel : MonoBehaviour 
+{	
+	void OnTriggerEnter2D (Collider2D other) 						//Checks if anything enteres the trigger area
 	{
-		if (other.tag == "Player")
+		if (other.tag == "Player")									//Checks if the object entering the trigger area is the player
 		{
-			Application.LoadLevel (Application.loadedLevel + 1);
-			Button.buttonActive = false;
+			Application.LoadLevel (Application.loadedLevel + 1);	//Calls the built-in function to laod a new level. Sets it to load the next level by getting the current level + 1
+			Button.buttonActive = false;							//Sets a boolean in the script "Button.cs" to lok alle buttons/doors again (so it does not stay true)
 		}
-
 	}
 }

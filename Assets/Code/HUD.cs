85b82328f303f335fcf1b5ac1689f0e3954f6797
Nadia Axel <nrasmu12@student.aau.dk>
2014-04-27 15:24:49 +0200

    Deleted useless stuff!

--- Assets/Code/HUD.cs
+++ Assets/Code/HUD.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class HUD : MonoBehaviour
 {
 	public GUISkin mySkin;
+	GUIStyle HealthDisplay;
+
 	bool hudOn;
 	float screenHeight = Screen.height;
 	float screenWidth = Screen.width;
@@ -11,13 +13,17 @@ public class HUD : MonoBehaviour
 
 	void OnGUI()
 	{
-		GUI.Box (new Rect(150  , screenHeight-600, 100,25),"Health: " + player.Health);
-		GUI.Box (new Rect(250  , screenHeight-600, 200,25), player.CurrentWeapon.getName() + " Damage: " + player.CurrentWeapon.getHitDamage());
-		GUI.Box (new Rect(250  , screenHeight-575, 200,25),"Durabillity: " + player.CurrentWeapon.getDura() + " Max: " + player.CurrentWeapon.getMAXDura());
-		GUI.Box (new Rect(450  , screenHeight-600, 160,25),"Helm Armor: " + player.HelmArmor + " Max: " + player.HelmMaxArmor);
-		GUI.Box (new Rect(450  , screenHeight-575, 160,25),"Shield Armor: " + player.ShieldArmor + " Max: " + player.ShieldMaxArmor);
-		GUI.Box (new Rect(900  , screenHeight-600, 100,25),"Lobsters: " + player.LobsterParts);
-		GUI.Box (new Rect(1000 , screenHeight-600, 150,25),"Weapon Parts: " + player.WeaponParts);
+		GUI.skin = mySkin;
+		HealthDisplay = mySkin.customStyles[0];
+
+		GUI.Box (new Rect(0, screenHeight - 600, 20,20 ),"", HealthDisplay);
+		GUI.Box (new Rect(30, screenHeight - 600, 100,25),"" + player.Health);
+		GUI.Box (new Rect(screenWidth + 250, screenHeight-600, 200,25), player.CurrentWeapon.getName() + " Damage: " + player.CurrentWeapon.getHitDamage());
+		GUI.Box (new Rect(screenWidth + 250, screenHeight-575, 200,25),"Durabillity: " + player.CurrentWeapon.getDura() + " Max: " + player.CurrentWeapon.getMAXDura());
+		GUI.Box (new Rect(screenWidth + 450, screenHeight-600, 160,25),"Helm Armor: " + player.HelmArmor + " Max: " + player.HelmMaxArmor);
+		GUI.Box (new Rect(screenWidth + 450, screenHeight-575, 160,25),"Shield Armor: " + player.ShieldArmor + " Max: " + player.ShieldMaxArmor);
+		GUI.Box (new Rect(screenWidth + 900, screenHeight-600, 100,25),"Lobsters: " + player.LobsterParts);
+		GUI.Box (new Rect(screenWidth + 1000, screenHeight-600, 150,25),"Weapon Parts: " + player.WeaponParts);
 	}
 
 

a45f64156a0c3ce69783fcd066dac9436a02d5cb
Nikolaj Thomsen <nikolaj.thomsen@gmail.com>
2014-04-28 00:01:15 +0200

    HUD bug
    HUD will not persist through multiple levels, it forgets wrahhs stats.

---
+++ Assets/HUDpos.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDpos : MonoBehaviour {
+
+	void Awake()
+	{
+		GameObject.Find ("HUD").transform.position = this.transform.position;
+	}
+}

ccb254385b1be46377d2da203e49a5da2f16e229
Nadia Axel <nrasmu12@student.aau.dk>
2014-04-27 16:44:48 +0200

    Optimized HUD and commented LOBSTER

--- Assets/Code/HUD.cs
+++ Assets/Code/HUD.cs
@@ -20,9 +20,9 @@ public class HUD : MonoBehaviour
 		GUI.Box (new Rect(screenWidth * 0.035f, screenHeight * 0.02f, 60,  50),"", HealthDisplay);
 		GUI.Box (new Rect(screenWidth * 0.02f,screenHeight * 0.015f, 100, 40),"" + player.Health);
 		GUI.Box (new Rect(screenWidth * 0.1f, screenHeight 	* 0.02f, 150, 40), player.CurrentWeapon.getName() + " Damage: " + player.CurrentWeapon.getHitDamage(), StatsDisplay);
-		GUI.Box (new Rect(screenWidth * 0.25f, screenHeight	* 0.02f, 150, 40),"Durabillity: " + player.CurrentWeapon.getDura() /*+ " Max: " + player.CurrentWeapon.getMAXDura()*/, StatsDisplay);
-		GUI.Box (new Rect(screenWidth * 0.40f, screenHeight	* 0.02f, 150, 40),"Helm Armor: " + player.HelmArmor /*+ " Max: " + player.HelmMaxArmor*/, StatsDisplay);
-		GUI.Box (new Rect(screenWidth * 0.55f, screenHeight	* 0.02f, 150, 40),"Shield Armor: " + player.ShieldArmor /*+ " Max: " + player.ShieldMaxArmor*/, StatsDisplay);
+		GUI.Box (new Rect(screenWidth * 0.25f, screenHeight	* 0.02f, 150, 40),"Durabillity: " + player.CurrentWeapon.getDura() + " / " + player.CurrentWeapon.getMAXDura(), StatsDisplay);
+		GUI.Box (new Rect(screenWidth * 0.40f, screenHeight	* 0.02f, 150, 40),"Helm Armor: " + player.HelmArmor + " / " + player.HelmMaxArmor, StatsDisplay);
+		GUI.Box (new Rect(screenWidth * 0.55f, screenHeight	* 0.02f, 150, 40),"Shield Armor: " + player.ShieldArmor + " / " + player.ShieldMaxArmor, StatsDisplay);
 		GUI.Box (new Rect(screenWidth * 0.7f, screenHeight	* 0.02f, 150, 40),"Lobster Parts: " + player.LobsterParts, StatsDisplay);
 		GUI.Box (new Rect(screenWidth * 0.85f, screenHeight	* 0.02f, 150, 40),"Weapon Parts: " + player.WeaponParts, StatsDisplay);
 	}
